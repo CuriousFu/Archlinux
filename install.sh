54c1409d25a96d18c10136048683b6c6e38be4da
CuriousFu <2269930627@qq.com>
2017-07-10 23:46:47 +0800

    new file:   install.sh 	new file:   py3status/weather_yahoo.py

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Created By:      Curious Fu <xkgjfl@gmail.com>
+# Created Time:    2017-07-10 23:43:49
+# Modified Time:   2017-07-10 23:45:39
+set -e
+sudo cp ./py3status/weather_yahoo.py /usr/lib/python3.6/site-packages/py3status/modules/
+
